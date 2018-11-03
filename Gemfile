0d6f94ed9f17dfbfeb46cf9f97f9b8f4da40ec8c
paramagician <konnor5456@gmail.com>
2018-11-03 19:12:57 +0000

    added rest_client code

--- Gemfile
+++ Gemfile
@@ -1,1 +1,3 @@
+# frozen_string_literal: true
+
 gem 'rest-client', '2.0.2'
--- rest_search.rb
+++ rest_search.rb
@@ -1,4 +1,17 @@
-require 'rest-client'
-require 'pry'
+# frozen_string_literal: true
 
-binding.pry
+require 'rest-client'
+
+def sanitize_search(input:)
+  input.split.join('+')
+end
+
+def search(input:)
+  input = sanitize_search(input: input)
+  RestClient.get("http://www.bing.com/search?q=#{input}")
+end
+
+puts 'What would you like to search?'
+input = gets.chomp
+results = search(input: input)
+puts results
